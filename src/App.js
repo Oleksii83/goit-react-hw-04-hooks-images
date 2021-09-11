d32e29cfbab8344da167ef76ddb9d76665fa731d
Oleksii83 <zaaytav@gmail.com>
2021-09-11 16:59:47 +0300

    task done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Searchbar from './Components/Searchbar/Searchbar';
 import ImageGallery from './Components/ImageGallery/ImageGallery';
 
-class App extends Component {
-  state = {
-    query: '',
-    loading: false,
+export default function App() {
+  const [query, setQuery] = useState('');
+  // const [loading, SetLoading] = useState(false)
+
+  const onSubmit = query => {
+    setQuery({ query });
   };
 
-  componentDidMount() {}
+  return (
+    <div className="App">
+      <Searchbar onSubmit={onSubmit} />
+      {/* {this.state.loading && <h1> Downloading... </h1>} */}
+      <ImageGallery query={query} />
 
-  onSubmit = query => {
-    this.setState({ query });
-    console.log(query);
-  };
-  render() {
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.onSubmit} />
-        {/* {this.state.loading && <h1> Downloading... </h1>} */}
-        <ImageGallery query={this.state.query} />
-
-        <ToastContainer autoClose={3000} position="top-left" />
-      </div>
-    );
-  }
+      <ToastContainer autoClose={3000} position="top-left" />
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     query: '',
+//     loading: false,
+//   };
+
+//   componentDidMount() {}
+
+//   onSubmit = query => {
+//     this.setState({ query });
+//     console.log(query);
+//   };
+//   render() {
+//     return (
+//       <div className="App">
+//         <Searchbar onSubmit={this.onSubmit} />
+//         {/* {this.state.loading && <h1> Downloading... </h1>} */}
+//         <ImageGallery query={this.state.query} />
+
+//         <ToastContainer autoClose={3000} position="top-left" />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import s from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Button from '../Button/Button';
@@ -7,120 +7,219 @@ import Modal from '../Modal/Modal';
 import API from '../services/gallery-api';
 import { toast } from 'react-toastify';
 
-class ImageGallery extends Component {
-  state = {
-    pictures: null,
-    error: null,
-    status: 'idle',
-    page: 1,
-    isModalOpen: false,
-    modalUrl: '',
-  };
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query } = this.props;
-    const { page } = this.state;
-    if (prevProps.query !== query) {
-      this.setState({ pictures: null, status: 'pending', page: 1 });
+export default function ImageGallery({ query }) {
+  const [pictures, setPictures] = useState(null);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const [page, setPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalUrl, setModalUrl] = useState('');
 
-      API.getPhoto(query, page)
-        .then(pictures => {
-          if (pictures.total === 0) {
-            return toast.error(`No result for ${query}. Try another query`);
-          }
-          this.setState({
-            pictures: pictures.hits,
-          });
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }))
-        .finally(() =>
-          this.setState(prevState => {
-            return { status: 'idle', page: prevState.page + 1 };
-          }),
-        );
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-  }
+    setStatus(Status.PENDING);
 
-  getNextPagePictures = () => {
-    const { page } = this.state;
-    const { query } = this.props;
+    API.getPhoto(query, 1)
+      .then(pictures => {
+        if (pictures.total === 0) {
+          return toast.error(`No result for "${query}". Try another query`);
+        }
+        setPictures(pictures.hits);
+      })
+      .catch(error => {
+        setError(error);
+        setStatus(Status.REJECTED);
+      })
+      .finally(() => {
+        setStatus(Status.RESOLVED);
+        setPage(2);
+      });
+  }, [query]);
 
-    this.setState({ status: 'pending' });
+  const getNextPagePictures = () => {
+    setStatus('pending');
 
     API.getPhoto(query, page)
-      .then(pictures => {
-        if (pictures.hits.length === 0) {
-          console.log('lenght 0');
+      .then(result => {
+        if (result.hits.length === 0) {
           toast.warning(`No more pictures for "${query}" query`);
           return;
         }
-        this.setState(prevState => {
-          return {
-            pictures: [...prevState.pictures, ...pictures.hits],
-            page: prevState.page + 1,
-          };
-        });
+        setPictures(pictures => [...pictures, ...result.hits]);
+
         window.scrollTo({
           top: document.documentElement.scrollHeight,
           behavior: 'smooth',
         });
       })
-      .catch(error => this.setState({ error, status: 'rejected' }))
-      .finally(() => this.setState({ status: 'idle' }));
+      .catch(error => {
+        setError(error);
+        setStatus(Status.REJECTED);
+      })
+      .finally(() => {
+        setPage(page + 1);
+      });
+    setStatus(Status.RESOLVED);
   };
 
-  toggleModal = url => {
-    this.setState(prevState => {
-      return {
-        isModalOpen: !prevState.isModalOpen,
-        modalUrl: url,
-      };
-    });
+  const toggleModal = url => {
+    setIsModalOpen(!isModalOpen);
+    setModalUrl(url);
   };
 
-  render() {
-    const { pictures, error, status, isModalOpen, modalUrl } = this.state;
+  if (status === Status.IDLE) {
+    return null;
+  }
 
-    if (status === 'idle') {
-      return (
-        <>
-          <ul className={s.ImageGallery}>
-            {pictures &&
-              pictures.map(picture => (
-                <ImageGalleryItem
-                  key={picture.id}
-                  url={picture.webformatURL}
-                  tags={picture.tags}
-                  modalUrl={picture.largeImageURL}
-                  onClick={this.toggleModal}
-                />
-              ))}
-          </ul>
-          {pictures && <Button onClick={this.getNextPagePictures} />}
-          {isModalOpen && <Modal onClose={this.toggleModal} url={modalUrl} />}
-        </>
-      );
-    }
+  if (status === Status.RESOLVED || status === Status.PENDING) {
+    return (
+      <>
+        <ul className={s.ImageGallery}>
+          {pictures &&
+            pictures.map(picture => (
+              <ImageGalleryItem
+                key={picture.id}
+                url={picture.webformatURL}
+                tags={picture.tags}
+                modalUrl={picture.largeImageURL}
+                onClick={toggleModal}
+              />
+            ))}
+        </ul>
+        {status === Status.PENDING && <Loader />}
+        {pictures && <Button onClick={getNextPagePictures} />}
+        {isModalOpen && <Modal onClose={toggleModal} url={modalUrl} />}
+      </>
+    );
+  }
 
-    if (status === 'rejected') {
-      return <h2>{error.message}</h2>;
-    }
-
-    if (status === 'pending') {
-      return (
-        <>
-          <ul className={s.ImageGallery}>
-            {pictures &&
-              pictures.map(picture => (
-                <ImageGalleryItem key={picture.id} url={picture.webformatURL} tags={picture.tags} />
-              ))}
-          </ul>
-          <Loader />
-          <Button onClick={this.getNextPagePictures} />
-        </>
-      );
-    }
+  if (status === Status.REJECTED) {
+    return <h2>{error.message}</h2>;
   }
 }
 
-export default ImageGallery;
+// class ImageGallery extends Component {
+//   state = {
+//     pictures: null,
+//     error: null,
+//     status: 'idle',
+//     page: 1,
+//     isModalOpen: false,
+//     modalUrl: '',
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const { query } = this.props;
+//     const { page } = this.state;
+//     if (prevProps.query !== query) {
+//       this.setState({ pictures: null, status: 'pending', page: 1 });
+
+//       API.getPhoto(query, page)
+//         .then(pictures => {
+//           if (pictures.total === 0) {
+//             return toast.error(`No result for ${query}. Try another query`);
+//           }
+//           this.setState({
+//             pictures: pictures.hits,
+//           });
+//         })
+//         .catch(error => this.setState({ error, status: 'rejected' }))
+//         .finally(() =>
+//           this.setState(prevState => {
+//             return { status: 'idle', page: prevState.page + 1 };
+//           }),
+//         );
+//     }
+//   }
+
+//   getNextPagePictures = () => {
+//     const { page } = this.state;
+//     const { query } = this.props;
+
+//     this.setState({ status: 'pending' });
+
+//     API.getPhoto(query, page)
+//       .then(pictures => {
+//         if (pictures.hits.length === 0) {
+//           console.log('lenght 0');
+//           toast.warning(`No more pictures for "${query}" query`);
+//           return;
+//         }
+//         this.setState(prevState => {
+//           return {
+//             pictures: [...prevState.pictures, ...pictures.hits],
+//             page: prevState.page + 1,
+//           };
+//         });
+//         window.scrollTo({
+//           top: document.documentElement.scrollHeight,
+//           behavior: 'smooth',
+//         });
+//       })
+//       .catch(error => this.setState({ error, status: 'rejected' }))
+//       .finally(() => this.setState({ status: 'idle' }));
+//   };
+
+//   toggleModal = url => {
+//     this.setState(prevState => {
+//       return {
+//         isModalOpen: !prevState.isModalOpen,
+//         modalUrl: url,
+//       };
+//     });
+//   };
+
+//   render() {
+//     const { pictures, error, status, isModalOpen, modalUrl } = this.state;
+
+//     if (status === 'idle') {
+//       return (
+//         <>
+//           <ul className={s.ImageGallery}>
+//             {pictures &&
+//               pictures.map(picture => (
+//                 <ImageGalleryItem
+//                   key={picture.id}
+//                   url={picture.webformatURL}
+//                   tags={picture.tags}
+//                   modalUrl={picture.largeImageURL}
+//                   onClick={this.toggleModal}
+//                 />
+//               ))}
+//           </ul>
+//           {pictures && <Button onClick={this.getNextPagePictures} />}
+//           {isModalOpen && <Modal onClose={this.toggleModal} url={modalUrl} />}
+//         </>
+//       );
+//     }
+
+//     if (status === 'rejected') {
+//       return <h2>{error.message}</h2>;
+//     }
+
+//     if (status === 'pending') {
+//       return (
+//         <>
+//           <ul className={s.ImageGallery}>
+//             {pictures &&
+//               pictures.map(picture => (
+//                 <ImageGalleryItem key={picture.id} url={picture.webformatURL} tags={picture.tags} />
+//               ))}
+//           </ul>
+//           <Loader />
+//           <Button onClick={this.getNextPagePictures} />
+//         </>
+//       );
+//     }
+//   }
+// }
+
+// export default ImageGallery;
